a4af1b44441217d8765333dbab14bbfcb1ac65c6
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2021-08-26 11:12:31 -0700

    Update callsmusic.py

--- TamilBots/services/callsmusic/callsmusic.py
+++ TamilBots/services/callsmusic/callsmusic.py
@@ -2,20 +2,20 @@ from pyrogram import Client
 from pytgcalls import PyTgCalls
 
 from TamilBots.config import API_HASH, API_ID, SESSION_NAME
-from TamilBots.services.callsmusic import queues
+from TamilBots.services.queues import queues
 
 client = Client(SESSION_NAME, API_ID, API_HASH)
 pytgcalls = PyTgCalls(client)
 
 
 @pytgcalls.on_stream_end()
-def on_stream_end(chat_id: int) -> None:
+async def on_stream_end(client: PyTgCalls, chat_id: int) -> None:
     queues.task_done(chat_id)
 
     if queues.is_empty(chat_id):
-        pytgcalls.leave_group_call(chat_id)
+        await pytgcalls.leave_group_call(chat_id)
     else:
-        pytgcalls.change_stream(chat_id, queues.get(chat_id)["file"])
+        await pytgcalls.change_stream(chat_id, queues.get(chat_id)["file"])
 
 
-run = pytgcalls.run
+run = pytgcalls.start
