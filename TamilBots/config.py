bdebb5295072ad9c1d3bd46ebaff8547c1adb238
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2021-08-29 06:38:45 -0700

    Update config.py

--- TamilBots/config.py
+++ TamilBots/config.py
@@ -25,5 +25,4 @@ ARQ_API_KEY = getenv("ARQ_API_KEY", None)
 PMPERMIT = getenv("PMPERMIT", None)
 LOG_GRP = getenv("LOG_GRP", None)
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
-
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
