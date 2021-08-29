9db79ffa849e287ff8d0ec914a2b14d0c12990b7
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2021-08-29 06:50:10 -0700

    Update __init__.py

--- TamilBots/services/callsmusic/__init__.py
+++ TamilBots/services/callsmusic/__init__.py
@@ -1,2 +1,6 @@
-from TamilBots.services.callsmusic import queues
-from TamilBots.services.callsmusic.callsmusic import pytgcalls, run
+from pyrogram import Client
+
+from TamilBots import config
+
+client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
+run = client.run
