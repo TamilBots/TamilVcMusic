fd29cf797b4542fcb75b894bd9582037d35ddbbb
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2021-09-19 19:05:46 +0530

    Update msg.py

--- TamilVc/modules/msg.py
+++ TamilVc/modules/msg.py
@@ -5,7 +5,7 @@ from TamilVc.config import PROJECT_NAME
 from TamilVc.config import SUPPORT_GROUP
 from TamilVc.config import UPDATES_CHANNEL
 class Messages():
-      START_MSG = "**Hello 👋 [{}](tg://user?id={})!**\n\n🤖 I am an advanced bot created for playing music in the voice chats of Telegram Groups & Channels.\n\n✅ Send me /help for more info."
+      START_MSG = "**Hello 👋 [{}](tg://user?id={})!**\n\n🤖 I am an advanced bot created for playing music in the voice chats of Telegram Groups & Channels.\n\n✅ Send me /help for more info. Report 🐛 Bugs At : @TamilSupport."
       HELP_MSG = [
         ".",
 f"""
